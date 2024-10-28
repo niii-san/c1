266de06ca633dc830854c785d9d8701fb6fe72e7
Nishan Bista <nishanbista525@gmail.com>
2024-10-29 01:23:02 +0545

    double pointers

--- programs/7_pointers.c
+++ programs/7_pointers.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
-//this function directly manipulates the original age instead of copying copying copying
-void updateAge(int *ageAddress,int newAge){
-
-    *ageAddress=newAge;
-
-}
+// this function directly manipulates the original age instead of copying
+// copying 
+void updateAge(int *ageAddress, int newAge) { *ageAddress = newAge; }
 
 int main() {
   printf("Hello From Pointer\n");
 
-    // * is called dereferencing operator, also used to create pointer variable
-    //  & is called referencing operator
+  // * is called dereferencing operator, also used to create pointer variable
+  //  & is called referencing operator
 
   int x = 10;
   int *pX = &x; // int *pX is a pointer variable, &x returns memory address of
@@ -20,13 +17,22 @@ int main() {
   printf("Memory address of x is: %p\n", pX);
   printf("Value thats in the address is: %d\n", *pX);
 
+  int age = 10;
+  printf("Age before %d\n", age);
+  updateAge(&age, 20);
+  printf("Age after: %d\n", age);
 
-    int age =10;
-    printf("Age before %d\n",age);
+  // Double pointer
+  printf("Pointer pointing to a pointer that is pointing to a variable!\n");
 
-    updateAge(&age,20);
+  int luckyNumber = 7;
+  int *pointerOfLuckyNumber = &luckyNumber;
+  int *(*pointerOfPointerOfLuckyNumber) = &pointerOfLuckyNumber;
 
-    printf("Age after: %d\n",age);
+  printf("Lucky number address %p\n", pointerOfLuckyNumber);
+  printf("Addrress of pointer that is pointing to luckyNumber: %p\n",
+         pointerOfPointerOfLuckyNumber);
+  printf("Actual luckyNumber: %d", *(*pointerOfPointerOfLuckyNumber));
 
 
   return 0;
